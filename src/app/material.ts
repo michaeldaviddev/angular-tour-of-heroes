07ead3caafe36a9d715c7bb8e7be38440bcda321
Michael David <mdavidmachacuay@gmail.com>
2019-01-23 12:11:40 -0500

    add material

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatListModule, MatChipsModule, MatCardModule, MatInputModule } from '@angular/material';
 
 @NgModule({
-    imports: [],
-    exports: [],
+    imports: [MatToolbarModule, MatListModule, MatChipsModule, MatCardModule, MatInputModule],
+    exports: [MatToolbarModule, MatListModule, MatChipsModule, MatCardModule, MatInputModule],
 })
 
 export class MaterialModule {}
